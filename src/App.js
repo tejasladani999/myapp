0f5fe1734e54cfacc2e5587340d7cdd4dcdef9bf
Assan Rabari <111674374+AssanRabari@users.noreply.github.com>
2023-09-29 01:06:23 +0530

    #CRUD_operation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Replace 'data.json' with the path to your JSON file
+    axios.get('http://localhost:3200/posts')
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // The empty dependency array ensures this effect runs only once, similar to componentDidMount
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  align="center" className='center-table'>
+      <h1>Employee Data</h1>
+      <table className='table table-hover' align='center' style={{width:"80%"}}>
+        <thead>
+          <tr>
+            <th>Department</th>
+            <th>Employee Name</th>
+            <th>Gender</th>
+            <th>Experience</th>
+            <th>DeptType</th>
+            <th colSpan={2}>Action  <button className='btn btn-outline-primary'>Add Employee</button></th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((employee, index) => (
+            <tr className={employee.deptType === 'Government' ? 'text-success' : 'text-danger'} key={index}>
+              <td>{employee.department}</td>
+              <td>{employee.employeeName}</td>
+              <td>{employee.gender}</td>
+              <td>{employee.experience}</td>
+              <td>{employee.deptType}</td>
+              <td>
+                {/* Add action buttons or links here */}
+                <button className='btn btn-outline-warning' style={{marginRight:"50px"}}>Edit</button>
+                <button className='btn btn-outline-danger'>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
